798692ee31cdfdc7259ed5203d38eaae67528c32
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2023-02-21 16:47:42 +0700

    data_create

--- Seminar_14_02_23/Task_43.py
+++ Seminar_14_02_23/Task_43.py
@@ -9,18 +9,16 @@
 # Вывод: 2
 print('Введите массив в строку: ')
 list_n = list(map(int, input().split())) 
-count=0
+count = 0
 for i in range(len(list_n)-1):
     if list_n[i] is None:
         continue
-    count_i=1
-    flag = True
-    for j in range(i+1,len(list_n)):
-
-        if list_n[j] is not None and list_n[i]==list_n[j]:
-            count_i+=1
-            list_n[j]=None
-    count+=count_i//2
-    print(count,count_i)
+    count_i = 1
+    for j in range(i + 1, len(list_n)):
+        if list_n[j] is not None and list_n[i] == list_n[j]:
+            count_i += 1
+            list_n[j] = None
+    count += count_i // 2
+    print(count, count_i)
 print(count)
 
